506305bea1e3080ccb42c5b724fc6d8f3e48d710
Heidi Pappila <heidipappila@kamk.fi>
2024-12-07 19:57:11 +0200

    Tiedoston nimiä muutettu ja sisältöä parennettu, sql kyselyt toimii nyt suoraan tietokantaan

--- app/src/🏠Etusivu.py
+++ app/src/🏠Etusivu.py
@@ -3,21 +3,23 @@ import streamlit as st
 # Päänäkymä
 st.set_page_config(
     page_title="Pääsivu",
-    page_icon="⭐️",
+    page_icon="🏠",
     layout="wide"
 )
 
 # Sivun otsikko ja teksti
-st.title("⚜️ Mojovat ⚜️")
+st.title("🚀 Mojovat: Ennusteita datan pohjalta ")
 st.write("Tämä on projektimme pääsivu, joka sisältää ... Tänne vois lisää myös GitLab stats")
 
 # Tekoälyllä luotu kuva
 st.image('/app/src/images/kuva.webp', caption='Kuva on luotu käyttäen OpenAI DALL·E 3 -työkalua.')
 
 # Repositorin linkki
-st.header("Repositori")
-st.link_button("GitLab repositorin linkki", "https://gitlab.dclabra.fi/projekti-2-ml-2024/mojovat/project_sigma")
+st.header("💾 Repositori")
+st.markdown("Tässä linkki GitLab projektin repositoriin")
+st.link_button("GitLab", "https://gitlab.dclabra.fi/projekti-2-ml-2024/mojovat/project_sigma")
 
 # Clockify linkki
-st.header("Clockify")
+st.header("⏱️ Clockify")
+st.markdown("Tässä Mojovien projektin yhteistunnit")
 st.markdown('<iframe src="https://app.clockify.me/shared/673c996fa5b53c67d258d043" width="1000" height="600"></iframe>', unsafe_allow_html=True)
